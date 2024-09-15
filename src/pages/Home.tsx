d0f63e335cc086d5fec878984f719991822e9684
Yiyangse <azziyang@naver.com>
2024-09-15 16:04:43 +0900

    CSS Theme 팔레트 연구

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
-import './Home.css'; // 커스텀 CSS 파일을 import하여 애니메이션 효과 적용
-import '../index.css'; 
+import React from "react";
+import "./Home.css"; // 커스텀 CSS 파일을 import하여 애니메이션 효과 적용
+import "../index.css";
+import Contents from "../components/Contents";
+
 const Home: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-teal-500 text-white text-center p-5">
-      <h2 className="text-6xl mb-4 animate-fadeIn">💭오늘 하루 어떠셨나요?</h2>
-      <br />
-      <br />
-      <h2 className="text-3xl animate-fadeIn animate-delay-500">단단한 나를 만들어주는<br />가장 쉬운 방법</h2>
-      <br />
-      <br />
-    </div>
+    <>
+      <div className="flex flex-col items-center justify-center h-screen bg-teal-500 text-white text-center p-5">
+        <h2 className="text-6xl mb-4 animate-fadeIn">
+          💭오늘 하루 어떠셨나요?
+        </h2>
+        <br />
+        <br />
+        <h2 className="text-3xl animate-fadeIn animate-delay-500">
+          단단한 나를 만들어주는
+          <br />
+          가장 쉬운 방법
+        </h2>
+        <br />
+        <br />
+      </div>
+      <div>
+        <Contents />
+      </div>
+    </>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class', // Enables dark mode support
   theme: {
     extend: {
       colors: {
-        teal: {
-          DEFAULT: '#48CFCB',
-          light: '#48CFCB', // 메인 teal 색상
-          dark: '#229799',  // 어두운 teal 색상
+        scampi: {
+          50: '#f5f6f9',
+          100: '#e8e9f1',
+          200: '#d6d8e7',
+          300: '#babed6',
+          400: '#989dc2',
+          500: '#8082b1',
+          600: '#6f6ea2',
+          700: '#5d5a88',
+          800: '#57537a',
+          900: '#484662',
+          950: '#2f2d3e',
         },
-        grey: {
-          DEFAULT: '#F5F5F5', // 밝은 회색
-          dark: '#424242',    // 어두운 회색
+        gray: {
+          // Customize or add more grays if necessary
         },
-        black: '#000000', // 검정
+        blue: {
+          // Customize or add more blues if necessary
+        },
+        green: {
+          // Customize or add more greens if necessary
+        },
+      },
+      keyframes: {
+        slide: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      animation: {
+        slide: 'slide 10s linear infinite',
       },
     },
   },
